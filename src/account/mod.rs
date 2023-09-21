6ff764e2f409bb2ef96a91163cc5d0a676736483
kebtech <64436886+kebtech@users.noreply.github.com>
2023-09-21 11:15:43 -0400

    updated account

---
+++ src/account/mod.rs
@@ -0,0 +1,72 @@
+use cnctd_rest::Rest;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use anyhow::anyhow;
+
+use crate::GitProvider;
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct GitAccount {
+    pub provider: GitProvider,
+    pub login: String,
+    pub token: String,
+    pub personal_url: String,
+    pub org_urls: Vec<String>,
+    pub default_url: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct GitUser {
+    pub login: String,
+}
+
+impl GitUser {
+    pub async fn get(provider: &GitProvider, token: &str) -> anyhow::Result<Self> {
+        match provider {
+            GitProvider::GitHub => {
+                let user: Self = Rest::get_with_auth("https://api.github.com/user", token).await?;
+                Ok(user)
+            }
+            _ => {
+                Err(anyhow!("Provider not supported yet"))
+            }
+        }   
+    }
+}
+
+impl GitAccount {
+    pub async fn new(provider: GitProvider, token: &str) -> anyhow::Result<Self> {
+        let login = GitUser::get(&provider, token).await?.login;
+
+        let personal_url = match provider {
+            GitProvider::GitHub => format!("https://github.com/{}", login),
+            GitProvider::GitLab => format!("https://gitlab.com/{}", login),
+            GitProvider::Bitbucket => format!("https://bitbucket.org/{}", login),
+        };
+        
+        // Fetch organizations based on the provider
+        let org_urls = match &provider {
+            GitProvider::GitHub => {
+                let orgs: Vec<Value> = Rest::get_with_auth("https://api.github.com/user/orgs", token).await?;
+                let urls: Vec<String> = orgs.iter()
+                    .map(|org| format!("https://github.com/{}", org["login"].as_str().unwrap()))
+                    .collect();
+                Ok(urls)
+            },
+            // Add logic for other providers here...
+            _ => Err(anyhow!("provider not yet supported")),
+        }?;
+
+
+        Ok(Self {
+            provider,
+            login: login.to_string(),
+            token: token.to_string(),
+            personal_url: personal_url.clone(),
+            org_urls,
+            default_url: personal_url,
+        })
+    }
+
+
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,89 +1,10 @@
-use git2::{RemoteCallbacks, Cred, Repository, StatusOptions};
-use serde_json::Value;
+use serde::{Deserialize, Serialize};
 
+pub mod account;
 
-
-pub struct Git {
-    token: String,
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub enum GitProvider {
+    GitHub,
+    GitLab,
+    Bitbucket,
 }
-
-impl Git {
-    pub fn new(token: String) -> Self {
-        Self { token }
-    } 
-
-    pub async fn list_all_repos(&self) -> Result<(), reqwest::Error> {
-        let url = "https://api.github.com/user/repos";
-        let client = reqwest::Client::new();
-        let response = client.get(url)
-            .header("Authorization", format!("token {}", self.token))
-            .header("User-Agent", "my-app")
-            .send()
-            .await?;
-
-        let repos: Value = response.json().await?;
-        println!("Repos: {:?}", repos);
-
-        Ok(())
-    }
-
-    pub fn test_git2_auth(&self, repo_url: &str) -> Result<(), git2::Error> {
-        let mut callbacks = RemoteCallbacks::new();
-        callbacks.credentials(|_url, username_from_url, _allowed_types| {
-            Cred::userpass_plaintext(username_from_url.unwrap(), &self.token)
-        });
-
-        let mut fo = git2::FetchOptions::new();
-        fo.remote_callbacks(callbacks);
-
-        // Open a repository in read-only mode
-        let repo = Repository::open_bare(repo_url)?;
-
-        // Fetch from the remote
-        let mut remote = repo.find_remote("origin")?;
-        remote.fetch(&["refs/heads/*:refs/heads/*"], Some(&mut fo), None)?;
-
-        Ok(())
-    }
-
-    pub fn status(repo_path: &str) {
-        // Open the repository
-        let repo = match Repository::open(repo_path) {
-            Ok(repo) => repo,
-            Err(e) => {
-                eprintln!("Could not open repository: {}", e);
-                return;
-            }
-        };
-    
-        // Prepare status options (this step is optional)
-        let mut opts = StatusOptions::new();
-        opts.include_untracked(true);
-    
-        // Get the statuses
-        let statuses = match repo.statuses(Some(&mut opts)) {
-            Ok(statuses) => statuses,
-            Err(e) => {
-                eprintln!("Could not get statuses");
-                return;
-            }
-        };
-    
-        // Loop through each status entry
-        for entry in statuses.iter().filter(|e| e.status() != git2::Status::CURRENT) {
-            let status = entry.status();
-    
-            if status.is_wt_new() {
-                println!("New file: {}", entry.path().unwrap_or("unknown"));
-            }
-            if status.is_wt_modified() {
-                println!("Modified file: {}", entry.path().unwrap_or("unknown"));
-            }
-            if status.is_wt_deleted() {
-                println!("Deleted file: {}", entry.path().unwrap_or("unknown"));
-            }
-            // Add more conditions here based on what you want to check
-        }
-    }
-}
-
